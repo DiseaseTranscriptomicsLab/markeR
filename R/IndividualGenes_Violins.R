09dbd607dbc0a983c2286dce936ece915f70a826
ritamtbsilva <ritamtbsilva@gmail.com>
2025-02-18 12:46:26 +0000

    add new functions to visualise individual genes using violins

---
+++ R/IndividualGenes_Violins.R
@@ -0,0 +1,173 @@
+#' Generate Violin Plots for Individual Genes
+#'
+#' This function creates violin plots of gene expression data with jittered points and optional faceting.
+#' It allows visualization of individual gene expression distributions across sample groups.
+#'
+#' @param data A data frame containing gene expression values with row names as gene names and column names as sample IDs. **(Required)**
+#' @param metadata An optional data frame containing sample metadata. The first column must match the sample IDs from `data`. **(Optional)**
+#' @param genes A character vector of gene names to be plotted. If more than 20 genes are provided, a warning is issued. **(Required)**
+#' @param GroupingVariable A character string specifying the column in `metadata` used for grouping samples on the x-axis. **(Required)**
+#' @param plot A logical value indicating whether to print the plot. If `FALSE`, only the output list is returned. Default is `TRUE`. **(Optional)**
+#' @param ncol An optional numeric value specifying the number of columns in the facet grid. If not provided, it is computed automatically. Only applicable if `divide` is `NULL`. **(Optional)**
+#' @param nrow An optional numeric value specifying the number of rows in the facet grid. If not provided, it is computed automatically. Only applicable if `divide` is `NULL`. **(Optional)**
+#' @param divide An optional character string specifying a column in `metadata` to be used for facetting, besides faceting by genes. **(Optional)**
+#' @param invert_divide A logical value indicating whether to invert the facet layout, when `divide` is being used. Default is `FALSE`, corresponding to genes in the rows. **(Optional)**
+#' @param ColorValues An optional named vector mapping unique values of `ColorVariable` to specific colors. If `NULL`, a default Brewer palette ("Paired") is used. **(Optional)**
+#' @param pointSize A numeric value specifying the size of the points in the plot. Default is `2`. **(Optional)**
+#' @param ColorVariable A character string specifying a metadata column used for coloring points. Default is `NULL`. **(Optional)**
+#' @param title A character string specifying the title of the plot. Default is `NULL`. **(Optional)**
+#' @param widthTitle A numeric value specifying the maximum width of the title before inserting line breaks. **(Optional)**
+#' @param y_limits A numeric vector of length 2 specifying the limits of the y-axis. If `NULL` (default), the y-axis is adjusted automatically. **(Optional)**
+#' @param legend_nrow A numeric value specifying the number of rows in the legend. Default is `NULL`. **(Optional)**
+#' @param xlab A character string specifying the x-axis label. If `NULL`, it defaults to `GroupingVariable`. **(Optional)**
+#' @param colorlab A character string specifying the legend title for colors. Default is an empty string. **(Optional)**
+#'
+#' @return A list containing:
+#'   \item{plot}{A ggplot2 object representing the facetted violin plots.}
+#'   \item{data}{A data frame used for plotting, including transformed expression values (log2) and metadata.}
+#'
+#' @details
+#' The function processes the gene expression data, filters for the specified genes, and transforms expression values using `log2()`.
+#' A violin plot with jittered points is generated using `ggplot2`. A median summary is added as a crossbar. If `divide` is provided,
+#' facets are created using `ggh4x::facet_grid2()`.
+#' Color customization is available via `ColorVariable` and `ColorValues`.
+#'
+#' @export
+#' @examples
+#' # Example dataset
+#' data <- data.frame(
+#'   A = c(10, 20, 30),
+#'   B = c(5, 15, 25),
+#'   C = c(2, 12, 22)
+#' )
+#' rownames(data) <- c("Gene1", "Gene2", "Gene3")
+#'
+#' metadata <- data.frame(
+#'   sample = c("A", "B", "C"),
+#'   Group = c("Control", "Treatment", "Control")
+#' )
+#'
+#' genes <- c("Gene1", "Gene2")
+#'
+#' IndividualGenes_Violins(data, metadata, genes, "Group")
+#'
+IndividualGenes_Violins <- function(data, metadata=NULL, genes,GroupingVariable, plot=TRUE, ncol=NULL, nrow=NULL, divide=NULL, invert_divide=FALSE, ColorValues=NULL, pointSize=2, ColorVariable=NULL,title=NULL, widthTitle=16,y_limits = NULL, legend_nrow = NULL,xlab=NULL, colorlab=NULL){
+
+  set.seed("1234")
+
+  if (length(genes) >20) warning("Number of genes is too high. Consider a smaller set of genes.")
+
+  # Wrap the signature name using the helper function
+  wrapped_title <- wrap_title(title, width = widthTitle)
+
+  datasub <- data[row.names(data) %in% genes,]
+  n <- nrow(data[row.names(data) %in% genes,])
+
+  datasub$gene <- row.names(datasub)
+  datasub <- reshape2::melt((datasub))
+  datasub$value <- log2(datasub$value)
+  colnames(datasub) <- c("gene","sample","expression")
+
+
+  if (!is.null(metadata)) colnames(metadata)[1] <- "sample"
+  if(!is.null(metadata)) datasub <- merge(datasub,metadata, by="sample")
+
+  # Determine grid layout
+  if (is.null(ncol) && is.null(nrow)) {
+
+    ncol <- ceiling(sqrt(n))
+    nrow <- ceiling(n / ncol)
+
+  } else if (is.null(ncol)){
+
+    ncol <- ceiling(n / nrow)
+
+  } else if (is.null(nrow)){
+
+    nrow <- ceiling(n / ncol)
+
+  }
+
+  plt <- ggplot2::ggplot(datasub, ggplot2::aes_string(y = "expression",x=GroupingVariable))
+
+  # Add jittered points, optionally colored by ColorVariable.Default: Brewer Pallette "Paired"
+  if (!is.null(ColorVariable)) {
+    plt <- plt + ggplot2::geom_jitter(ggplot2::aes_string(color = ColorVariable), size = pointSize, alpha = 0.5)
+  } else {
+    plt <- plt + ggplot2::geom_jitter(size = pointSize, alpha = 0.5) + ggplot2::scale_color_brewer(palette = "Paired")
+  }
+
+
+  # If ColorValues is provided, use a manual color scale; otherwise, if ColorVariable is provided,
+  # use a default brewer palette.
+  if (!is.null(ColorValues)) {
+    plt <- plt + ggplot2::scale_color_manual(values = ColorValues)
+  } else if (!is.null(ColorVariable)) {
+    plt <- plt + ggplot2::scale_color_brewer(palette = "Paired")
+  }
+
+  # Adjust legend rows if legend_nrow is specified
+  if (!is.null(legend_nrow)) {
+    plt <- plt + ggplot2::guides(color = ggplot2::guide_legend(nrow = legend_nrow))
+  }
+
+  # Add violin
+  plt <- plt + ggplot2::geom_violin(alpha=0.4)
+
+  # Add median summary crossbar.
+  plt <- plt + ggplot2::stat_summary(fun = median, fun.min = median, fun.max = median,
+                                 geom = "crossbar", width = 0.25,
+                                 position = ggplot2::position_dodge(width = 0.13))
+
+  # add second layer of facet, if any
+  if (!is.null(divide)){
+    if (!invert_divide){
+      plt <- plt +
+        ggh4x::facet_grid2( gene ~ .data[[divide]],
+                            scales = "free",
+                            independent= "y")
+    } else {
+      plt <- plt +
+        ggh4x::facet_grid2( .data[[divide]] ~ gene,
+                            scales = "free",
+                            independent= "y") # labeller = label_wrap_gen(width = 2, multi_line = TRUE)
+    }
+
+  } else{
+
+    plt <- plt +
+      ggplot2::facet_wrap( . ~ gene,
+                           scales = "free",
+                           ncol=ncol,
+                           nrow=nrow)
+
+  }
+
+  # If y_limits is specified, crop the plot without adjusting the data (violins).
+
+  if (!is.null(y_limits)) {
+    plt <- plt + ggplot2::coord_cartesian(ylim = y_limits)
+  }
+
+  # Change labels
+  if(is.null(xlab)){
+    xlab <- GroupingVariable
+  } else if (is.null(colorlab)){
+    colorlab <- ""
+  }
+
+  plt <- plt + ggplot2::labs(title = title, color = colorlab, x = xlab, y = "Expression (log2CPM)")
+
+  # change theme
+  plt <- plt + theme_classic() +
+    theme(axis.text.x = element_text(angle = 45, hjust=1),
+          plot.title = element_text(hjust = 0.5),
+          legend.position = "bottom")
+
+  if(plot){
+    print(plt)
+  }
+
+  invisible(list(plot=plt,
+                 data=datasub))
+}
---
+++ R/VisualiseIndividualGenes.R
@@ -0,0 +1,121 @@
+#' Visualize Individual Genes' Expression Using Various Methods
+#'
+#' This function provides multiple visualization methods for analyzing individual genes
+#' from an expression dataset. Users can generate violin plots, expression heatmaps,
+#' correlation heatmaps, ROC curves, and effect size plots.
+#'
+#' @param data A data frame where rows correspond to genes and columns correspond to samples.
+#'   The row names should be gene identifiers.
+#' @param metadata A data frame containing sample metadata. The first column should contain
+#'   sample identifiers matching the column names of `data`.
+#' @param genes A character vector of gene names to be plotted. If more than 20 genes are provided, a warning is issued.
+#' @param method A character string specifying which visualization method to use.
+#'   Options are `"violins"`, `"expr_heatmap"`, `"corr_heatmap"`, `"roc"`, `"effectsize"`, or `"all"`.
+#'   Default is `"violins"`. If `"all"` is selected, all methods will be applied.
+#' @param plot A logical value indicating whether to display the generated plots (`TRUE`) or not (`FALSE`).
+#'   Default is `TRUE`.
+#' @param GroupingVariable A character string specifying the column name in `metadata` to be used
+#'   for grouping samples on the x-axis in violin plots.
+#' @param ColorVariable A character string specifying the column name in `metadata` used
+#'   to color the points in violin plots.
+#' @param ColorValues An optional named vector mapping unique values of `ColorVariable` to specific colors.
+#' @param pointSize A numeric value specifying the size of jittered points in violin plots. Default is `2`.
+#' @param title An optional character string specifying the title of the plot.
+#' @param widthTitle An optional integer specifying the maximum width of the title before inserting line breaks. Default is `16`.
+#' @param y_limits A numeric vector of length 2 specifying the limits of the y-axis in violin plots.
+#'   If `NULL`, the y-axis is adjusted automatically. Default is `NULL`.
+#' @param legend_nrow A numeric value specifying the number of rows in the legend for violin plots.
+#'   If `NULL`, the default number of rows is determined automatically.
+#' @param ncol An optional numeric value specifying the number of columns in the grid layout for violin plots.
+#'   If `NULL`, it is automatically determined.
+#' @param nrow An optional numeric value specifying the number of rows in the grid layout for violin plots.
+#'   If `NULL`, it is automatically determined.
+#' @param divide A character string specifying a column in `metadata` used to divide the violin plots into facets.
+#'   Default is `NULL`.
+#' @param invert_divide A logical value indicating whether to invert the facet layout of violin plots.
+#'   Default is `FALSE`.
+#' @param xlab An optional character string specifying the label for the x-axis in violin plots.
+#'   Default is the name of `GroupingVariable`.
+#' @param colorlab An optional character string specifying the legend title for the color variable.
+#'   Default is `NULL`.
+#'
+#' @return A named list where each selected method (e.g., `"violins"`, `"expr_heatmap"`, etc.) is a key.
+#'   Each element in this list is itself a named list with the following structure:
+#'   \describe{
+#'     \item{`data`}{A data frame or matrix containing the transformed data used for plotting, formatted according to the selected method.}
+#'     \item{`plot`}{The generated plot object (e.g., a ggplot2 object) corresponding to the visualization.}
+#'     \item{`aux`}{Additional parameters, specific of each method.}
+#'   }
+#'   If `plot = TRUE`, the plots are also displayed in the output.
+#'
+#' @details
+#' This function calls multiple sub-functions based on the selected visualization method:
+#'
+#' - `IndividualGenes_Violins()`: Generates violin plots for gene expression (log2 transformed).
+#' - `IndividualGenes_ExprHeatmap()`: Creates a heatmap of expression values.
+#' - `IndividualGenes_CorrHeatmap()`: Computes and visualizes a correlation heatmap.
+#' - `IndividualGenes_ROC()`: Plots receiver operating characteristic (ROC) curves.
+#' - `IndividualGenes_EffectSize()`: Computes and visualizes effect sizes.
+#'
+#' The function returns a list of plots. If `method = "all"`, all available visualizations are included.
+#'
+#' @export
+VisualiseIndividualGenes <- function(data, metadata, gene_sets,
+                                     method = c("violins","expr_heatmap","corr_heatmap","roc","effectsize","all"),
+                                     plot = TRUE,
+                                     GroupingVariable = NULL,
+                                     ColorVariable = NULL,
+                                     ColorValues = NULL,
+                                     pointSize = 2,
+                                     title = NULL,
+                                     widthTitle = 16,
+                                     y_limits = NULL,
+                                     legend_nrow = NULL,
+                                     ncol = NULL,
+                                     nrow = NULL,
+                                     divide = NULL,
+                                     invert_divide = FALSE,
+                                     xlab = NULL,
+                                     colorlab = NULL) {
+
+  method <- match.arg(method)  # Validate method input
+
+  if (!is.data.frame(data)) stop("Error: data must be a data-frame")
+  if (!is.null(metadata) && !is.data.frame(metadata)) stop("Error: metadata must be a data-frame")
+  if (!is.list(gene_sets)) stop("Error: gene_sets must be a list")
+
+  ResultsList <- list()
+
+  if (method == "violins" | method == "all") {
+    ResultsList[["violins"]] <- IndividualGenes_Violins(data = data, metadata = metadata,
+                                                        genes = genes, GroupingVariable = GroupingVariable,
+                                                        ColorVariable = ColorVariable, ColorValues = ColorValues,
+                                                        pointSize = pointSize, title = title,
+                                                        widthTitle = widthTitle, y_limits = y_limits,
+                                                        legend_nrow = legend_nrow, ncol = ncol,
+                                                        nrow = nrow, divide = divide, invert_divide = invert_divide,
+                                                        xlab = xlab, colorlab = colorlab, plot = plot)
+  }
+
+  if (method == "expr_heatmap" | method == "all") {
+    ResultsList[["expr_heatmap"]] <- IndividualGenes_ExprHeatmap(data = data, metadata = metadata,
+                                                                 genes = genes, plot = plot)
+  }
+
+  if (method == "corr_heatmap" | method == "all") {
+    ResultsList[["corr_heatmap"]] <- IndividualGenes_CorrHeatmap(data = data, metadata = metadata,
+                                                                 genes = genes, plot = plot)
+  }
+
+  if (method == "roc" | method == "all") {
+    ResultsList[["roc"]] <- IndividualGenes_ROC(data = data, metadata = metadata,
+                                                genes = genes, plot = plot)
+  }
+
+  if (method == "effectsize" | method == "all") {
+    ResultsList[["effectsize"]] <- IndividualGenes_EffectSize(data = data, metadata = metadata,
+                                                              genes = genes, plot = plot)
+  }
+
+  return(ResultsList)
+}
