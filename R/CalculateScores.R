715c7818b4be22fd58c41a50b5d693b24949919c
ritamtbsilva <ritamtbsilva@gmail.com>
2025-03-05 18:16:48 +0000

    parameter all for CalculateScores

--- R/CalculateScores.R
+++ R/CalculateScores.R
@@ -5,7 +5,7 @@
 #'
 #' \describe{
 #' This function calculates a gene signature score for each sample based on one or more predefined gene sets
-#' (signatures). Two methods are available:
+#' (signatures). Four methods are available:
 #'
 #'   \item{\code{ssGSEA}}{
 #'     Uses the single-sample Gene Set Enrichment Analysis (ssGSEA) method to compute an enrichment score
@@ -20,9 +20,13 @@
 #'     Computes gene signature scores for each sample by ranking the expression of signature genes
 #'     in the dataset and normalizing the score based on the total number of genes.
 #'   }
+#'   \item{\code{all}}{
+#'     Computes gene signature scores using all three methods (\code{ssGSEA}, \code{logmedian}, and \code{ranking}).
+#'     The function returns a list containing the results of each method.
+#'   }
 #' }
 #'
-#' @param data A data frame of normalised (non-transformed) counts where each row is a gene and each column is a sample. The row names should contain gene names
+#' @param data A data frame of normalized (non-transformed) counts where each row is a gene and each column is a sample. The row names should contain gene names,
 #'   and the column names should contain sample identifiers. **(Required)**
 #' @param metadata A data frame describing the attributes of each sample. Each row corresponds to a sample and each column to an attribute.
 #'   The first column of \code{metadata} should be the sample identifiers (i.e., the column names of \code{data}).
@@ -38,16 +42,16 @@
 #' - The **second column** should indicate the expected direction of enrichment (1 for upregulated genes, -1 for downregulated genes).
 #'
 #' @param method A character string indicating the scoring method to use. Options are \code{"ssGSEA"},
-#' \code{"logmedian"} or \code{"ranking"}. Defaults to \code{"logmedian"}.
+#' \code{"logmedian"}, \code{"ranking"}, or \code{"all"} (to compute scores using all methods). Defaults to \code{"logmedian"}.
 #'
-#' @return A list containing the calculated scores for each gene signature. Each element of the list
-#' corresponds to one signature (named accordingly) and is a data frame with the following attributes:
+#' @return If a single method is chosen, a data frame containing the calculated scores for each gene signature, including metadata if provided.
+#' If \code{method = "all"}, a list is returned where each element corresponds to a scoring method and contains the respective data frame of scores.
+#'
 #' \describe{
 #'   \item{sample}{The sample identifier (matching the column names of the input data).}
 #'   \item{score}{The calculated gene signature score for the corresponding sample.}
 #'   \item{(metadata)}{Any additional columns from the \code{metadata} data frame provided by the user, if available.}
 #' }
-#'
 #'
 #' @examples
 #' \dontrun{
@@ -67,10 +71,16 @@
 #'   # Using the logmedian method (default):
 #'   scores_logmedian <- calculate_signature_score(data = gene_data,
 #'                                                 gene_sets = gene_sets)
+#'
+#'   # Using all methods:
+#'   scores_all <- calculate_signature_score(data = gene_data,
+#'                                           metadata = sample_metadata,
+#'                                           gene_sets = gene_sets,
+#'                                           method = "all")
 #' }
 #'
 #' @export
-CalculateScores <- function(data, metadata, gene_sets, method = c("ssGSEA", "logmedian","ranking")) {
+CalculateScores <- function(data, metadata, gene_sets, method = c("ssGSEA", "logmedian","ranking", "all")) {
 
   method <- match.arg(method)  # Validate method input
 
@@ -89,5 +99,11 @@ CalculateScores <- function(data, metadata, gene_sets, method = c("ssGSEA", "log
     return(CalculateScores_logmedian(data, metadata, gene_sets))
   } else if (method == "ranking"){
     return(CalculateScores_Ranking(data, metadata, gene_sets))
+  } else if (method == "all"){
+    return(list(ssGSEA=CalculateScores_ssGSEA(data, metadata, gene_sets),
+                logmedian=CalculateScores_logmedian(data, metadata, gene_sets),
+                ranking=CalculateScores_Ranking(data, metadata, gene_sets)))
+
   }
+
 }
