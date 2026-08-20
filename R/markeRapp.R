707797fca74f3b4483bec526ffae08a3ef84ca5c
Rita Silva <55759937+ritamtbsilva@users.noreply.github.com>
2026-08-20 15:25:18 +0100

    0.99.9

--- R/markeRapp.R
+++ R/markeRapp.R
@@ -47,7 +47,7 @@ ui <- bslib::page_navbar(
     #   target = "_blank",
     #   style = "font-size: 0.85em; color: #949494; text-decoration: none;"
     # )
-    shiny::a("Web App 0.99.8",
+    shiny::a("Web App 0.99.9",
              style = "font-size: 0.7em; color: #949494; text-decoration: none;")
   ),
   
